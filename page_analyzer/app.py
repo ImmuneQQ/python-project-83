dd1d905db6b141bdf128de7d15833ed2e764b67b
ImmuneQQ <vitalyasatka@gmail.com>
2023-01-02 14:13:28 +0500

    add index

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for
+)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'test'
+    return render_template('index.html')
+
+
+@app.get('/urls')
+def urls_get():
+    return render_template('urls.html')
+
+
+@app.post('/urls')
+def urls_post():
+    return redirect(url_for('urls_get'))
