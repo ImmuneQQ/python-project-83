d408050a10a6317fa4e3e05e26b610129721b76f
ImmuneQQ <vitalyasatka@gmail.com>
2023-01-03 03:25:41 +0500

    add ability to add sites

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,10 +2,24 @@ from flask import (
     Flask,
     render_template,
     redirect,
-    url_for
+    url_for,
+    request,
+    flash,
+    get_flashed_messages
 )
+import psycopg2
+import os
+from datetime import datetime
+import validators
+from urllib.parse import urlparse
+
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
+conn.set_session(autocommit=True)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
 
 
 @app.route('/')
@@ -15,9 +29,60 @@ def index():
 
 @app.get('/urls')
 def urls_get():
-    return render_template('urls.html')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM urls ORDER BY id DESC;")
+    sites = cur.fetchall()
+    return render_template('urls.html', sites=sites)
 
 
 @app.post('/urls')
 def urls_post():
-    return redirect(url_for('urls_get'))
+    url = request.form['url']
+    url_is_valid = validators.url(url)
+    url_is_empty = len(url) == 0
+    url_is_too_long = len(url) > 255
+    time_now = datetime.now()
+
+    if url_is_valid and not url_is_empty and not url_is_too_long:
+        parsed_url = urlparse(url)
+        norm_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM urls WHERE name='{norm_url}';")
+        found_site = cur.fetchone()
+        if found_site:
+            flash('Страница уже существует', 'alert-info')
+        else:
+            cur.execute(f"""INSERT INTO urls (name, created_at)
+                            VALUES ('{norm_url}', '{time_now}');""")
+            cur.execute(f"SELECT * FROM urls WHERE name='{norm_url}';")
+            found_site = cur.fetchone()
+            flash('Страница успешно добавлена', 'alert-success')
+        messages = get_flashed_messages(with_categories=True)
+        return redirect(url_for('url_item', id=found_site[0]))
+    else:
+        if not url_is_valid:
+            flash('Некорректный URL', 'alert-danger')
+        if url_is_empty:
+            flash('URL обязателен', 'alert-danger')
+        if url_is_too_long:
+            flash('URL превышает 255 символов', 'alert-danger')
+        messages = get_flashed_messages(with_categories=True)
+        return render_template(
+            'index.html',
+            url=url, messages=messages
+            ), 422
+
+
+@app.get('/urls/<id>')
+def url_item(id):
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM urls WHERE id={id};")
+    site = cur.fetchone()
+    name = site[1]
+    created_at = site[2].date()
+    return render_template(
+        'url_item.html',
+        id=id,
+        name=name,
+        created_at=created_at
+        )
