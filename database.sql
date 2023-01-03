0d8b120736415350dc9b0d434668b37dc04ec191
ImmuneQQ <vitalyasatka@gmail.com>
2023-01-03 10:29:40 +0500

    fix for hexlet tests

--- database.sql
+++ database.sql
@@ -11,8 +11,8 @@ CREATE TABLE url_checks (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id int REFERENCES urls (id),
     status_code int,
-    h1 text,
-    title text,
-    description text,
+    h1 varchar(255),
+    title varchar(255),
+    description varchar(255),
     created_at timestamp
 );
